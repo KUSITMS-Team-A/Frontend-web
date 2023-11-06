9ccb396f30669bf9934522caa5cafa756d3ee896
SuJin <kimsujin001127@gmail.com>
2023-11-06 16:07:09 +0900

    feat: 기본 컴포넌트 생성

---
+++ components/styles/Search.styles.tsx
@@ -0,0 +1,65 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div``;
+
+export const TitleBox = styled.div``;
+
+export const MiddleBox = styled.div`
+  display: flex;
+`;
+
+export const FiltersBox = styled.div`
+  display: flex;
+`;
+
+export const FilterComponentBox = styled.div``;
+
+export const SearchBox = styled.div``;
+
+export const SearchExpandBox = styled.div``;
+
+export const HeartBox = styled.div`
+  display: flex;
+`;
+
+export const HeartIconBox = styled.div``;
+
+export const HeartTextBox = styled.div``;
+
+export const MainBox = styled.div`
+  display: flex;
+`;
+
+export const MapBox = styled.div``;
+
+export const ListsBox = styled.div``;
+
+export const ListBox = styled.div``;
+
+export const TypeImgBox = styled.div``;
+
+export const InfoContainerBox = styled.div``;
+
+export const InfoTopBox = styled.div`
+  display: flex;
+`;
+
+export const NameBox = styled.div``;
+
+export const StoreTypeBox = styled.div``;
+
+export const WarningBox = styled.div``;
+
+export const StoreHeartBox = styled.div``;
+
+export const StoreInfoBox = styled.div``;
+
+export const StoreBottomBox = styled.div`
+  display: flex;
+`;
+
+export const StoreLocationBox = styled.div``;
+
+export const StoreDistanceBox = styled.div``;
+
+export const DownBtnBox = styled.div``;
--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -1,7 +1,49 @@
+import * as styles from "@/components/styles/Search.styles";
+
 export default function SearchHome() {
   return (
-    <>
-     
-    </>
-  )
+    <styles.Container>
+      <styles.TitleBox>가게 찾기</styles.TitleBox>
+      <styles.MiddleBox>
+        <styles.FiltersBox>
+          <styles.FilterComponentBox>전체</styles.FilterComponentBox>
+          <styles.FilterComponentBox>음식점</styles.FilterComponentBox>
+          <styles.FilterComponentBox>카페</styles.FilterComponentBox>
+          <styles.FilterComponentBox>미용</styles.FilterComponentBox>
+          <styles.FilterComponentBox>문화</styles.FilterComponentBox>
+          <styles.FilterComponentBox>기타</styles.FilterComponentBox>
+        </styles.FiltersBox>
+        {/** TODO: 누르면 expand로 변경되도록 */}
+        <styles.SearchBox></styles.SearchBox>
+        <styles.HeartBox>
+          <styles.HeartIconBox></styles.HeartIconBox>
+          <styles.HeartTextBox>픽한 업체</styles.HeartTextBox>
+        </styles.HeartBox>
+      </styles.MiddleBox>
+      <styles.MainBox>
+        <styles.MapBox></styles.MapBox>
+        <styles.ListsBox>
+          <styles.ListBox>
+            <styles.TypeImgBox></styles.TypeImgBox>
+            <styles.InfoContainerBox>
+              <styles.InfoTopBox>
+                <styles.NameBox>릴즈</styles.NameBox>
+                <styles.StoreTypeBox>음식점</styles.StoreTypeBox>
+                {/** TODO: warning 여부에 따라 보이도록
+                 * <styles.WarningBox></styles.WarningBox>
+                 */}
+                <styles.StoreHeartBox></styles.StoreHeartBox>
+              </styles.InfoTopBox>
+              <styles.StoreInfoBox>아메리칸</styles.StoreInfoBox>
+              <styles.StoreBottomBox>
+                <styles.StoreLocationBox>서울특별시</styles.StoreLocationBox>
+                <styles.StoreDistanceBox>176m</styles.StoreDistanceBox>
+              </styles.StoreBottomBox>
+              <styles.DownBtnBox></styles.DownBtnBox>
+            </styles.InfoContainerBox>
+          </styles.ListBox>
+        </styles.ListsBox>
+      </styles.MainBox>
+    </styles.Container>
+  );
 }
