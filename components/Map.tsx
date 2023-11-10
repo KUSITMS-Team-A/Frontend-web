c323d5f1c3bc4203e1335460601875a0a18bce64
SuJin <kimsujin001127@gmail.com>
2023-11-10 22:53:21 +0900

    fix: 지도 안뜨는 문제 해결

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,8 +1,5 @@
-import Script from "next/script";
 import { Map } from "react-kakao-maps-sdk";
 import { ReactNode } from "react";
-
-const KAKAO_SDK_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_KEY}&autoload=false`;
 
 interface Position {
   latitude: number;
@@ -13,7 +10,6 @@ interface Position {
 const KakaoMap = ({ latitude, longitude, children }: Position) => {
   return (
     <>
-      <Script src={KAKAO_SDK_URL} strategy="beforeInteractive" />
       <Map
         center={{ lat: latitude, lng: longitude }}
         level={5}
