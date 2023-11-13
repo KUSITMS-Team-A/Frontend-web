be1e891058117d72944feb7d057efbb003a10fac
SuJin <kimsujin001127@gmail.com>
2023-11-14 00:56:18 +0900

    style: 타이틀 디자인 구현 완성

--- components/styles/Contact.styles.tsx
+++ components/styles/Contact.styles.tsx
@@ -1,17 +1,53 @@
 import { COLORS } from "@/styles/colors";
 import styled from "@emotion/styled";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 920px;
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+`;
 
-export const TopBox = styled.div``;
+export const TopBox = styled.div`
+  display: flex;
+`;
 
 export const TopTitleBox = styled.div``;
 
-export const HeadTitleBox = styled.div``;
+export const HeadTitleBox = styled.div`
+  color: var(--, #1c1c1e);
+  font-size: 26px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 16.53px; /* 63.577% */
+  letter-spacing: -1.56px;
+`;
 
-export const SubTitleBox = styled.div``;
+export const SubTitleBox = styled.div`
+  margin-top: 12px;
+  color: var(--, #1c1c1e);
+  font-size: 15px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 19.264px; /* 128.425% */
+  letter-spacing: -0.9px;
+`;
 
-export const BlackButtonBox = styled.div``;
+export const BlackButtonBox = styled.div`
+  display: flex;
+  width: 118px;
+  height: 41.502px;
+  justify-content: center;
+  align-items: center;
+  margin-left: auto;
+  border-radius: 3.933px;
+  border: 1.311px solid var(--g1, #c6c6c6);
+  background: #505050;
+  color: #fff;
+  font-size: 18.335px;
+  font-weight: 400;
+  letter-spacing: -1.1px;
+`;
 
 export const MiddleBox = styled.div``;
 
