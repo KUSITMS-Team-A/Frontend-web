ac068f2a5873795eff1002e96bcf95821ccb9087
Ethan Lim <studentlimsoo@gmail.com>
2023-11-18 20:58:27 +0900

    feat : Header 화면 구현

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,3 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-
 export default function Home() {
   return <>이것은 랜딩페이지입니다</>;
 }
