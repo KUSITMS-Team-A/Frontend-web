b9225a5215fa7814cf009ad19b6959481f6fd378
SuJin <kimsujin001127@gmail.com>
2023-11-18 21:39:16 +0900

    feat: 컴포넌트 구성

---
+++ components/styles/PopUp.styles.tsx
@@ -0,0 +1,34 @@
+import { COLORS } from "@/styles/colors";
+import styled from "@emotion/styled";
+
+export const Container = styled.div``;
+
+export const TitleBox = styled.div``;
+
+export const SubTitleBox = styled.div``;
+
+export const ContentBox = styled.div``;
+
+export const TableTitleContainer = styled.div``;
+
+export const TableCountBox = styled.div``;
+
+export const TableContentTitle = styled.div``;
+
+export const TablePeriodTitle = styled.div``;
+
+export const PopUpsContainer = styled.div``;
+
+export const PopupContainer = styled.div``;
+
+export const NumberBox = styled.div``;
+
+export const PopupTitle = styled.div``;
+
+export const PopupPeriod = styled.div``;
+
+export const ButtonBox = styled.div``;
+
+export const EnrollBtn = styled.div``;
+
+export const RemoveBtn = styled.div``;
