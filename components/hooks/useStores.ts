3387308621d4daf30ea5404b427e44bc95d097b9
Ethan Lim <studentlimsoo@gmail.com>
2023-11-23 17:14:45 +0900

    feat : 가게 조회 api 및 훅 추가

---
+++ components/hooks/useStores.ts
@@ -0,0 +1,36 @@
+import { getFoods } from "@/pages/api/others";
+import { useEffect, useState } from "react";
+
+interface CouponProps {
+  isPicked: boolean;
+  name: string;
+  category: "FOOD" | "CAFE" | "BEAUTY" | "CULTURE" | "ETC";
+  pageSize: number;
+  pageNumber: number;
+}
+
+export const useStores = async ({
+  isPicked,
+  name,
+  category,
+  pageSize,
+  pageNumber,
+}: CouponProps) => {
+  const [data, setData] = useState<CouponProps | null>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await getFoods({
+        isPicked,
+        name,
+        category,
+        pageSize,
+        pageNumber,
+      });
+
+      console.log(`FOODS ${JSON.stringify(result)}`);
+    };
+
+    fetchData();
+  });
+};
