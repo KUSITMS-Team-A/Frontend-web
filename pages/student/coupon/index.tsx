907978849594fbe7d55e37ce46590ccfc1a870b2
Ethan Lim <studentlimsoo@gmail.com>
2023-11-23 20:44:43 +0900

    [#57] feat: 쿠폰 미리보기 추가 (#62)
    * fix : 쿠폰 중복 import 삭제
    
    * fix : 등록 후 뒤로가기
    
    * feat : 쿠폰 미리보기 추가
    
    * feat : 쿠폰 미리보기 추가

--- pages/student/coupon/index.tsx
+++ pages/student/coupon/index.tsx
@@ -1,7 +1,8 @@
 import { useRouter } from "next/router";
-import React, { useEffect, useState } from "react";
-import * as styles from "@/components/styles/popup/style";
 
+import React, { useState } from "react";
+
+import * as styles from "@/components/styles/popup/style";
 import { css } from "@emotion/css";
 import { useCouponData } from "@/components/hooks/useCouponData";
 import Link from "next/link";
@@ -75,7 +76,6 @@ const CouponAdminPage: React.FC = () => {
                 </styles.ListElement>
               );
             })
-
           ) : (
             <div
               className={css`
