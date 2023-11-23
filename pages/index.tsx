2e32ed3fc4703b15d0db54098286c8c526c0a1c2
Ethan Lim <studentlimsoo@gmail.com>
2023-11-23 21:07:03 +0900

    style : 백그라운드 임시 gif 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import Head from "next/head";
 
 import styled from "@emotion/styled";
-
+import Background from "@/assets/svg/background.gif";
 export default function Home() {
   return (
     <>
       <Head>
-        <title>제대로, ~~~~~~</title>
+        <title>
+          제대로,학생과 학생회, 가게 모두의 소소한 행복이 되는 제휴사업이 되도록
+        </title>
       </Head>
       <Container>
         <LandingTitle>학생과 학생회, 가게 모두의</LandingTitle>
@@ -14,7 +16,6 @@ export default function Home() {
       </Container>
     </>
   );
-
 }
 
 const Container = styled.div`
@@ -26,6 +27,11 @@ const Container = styled.div`
 
   padding-top: 5%;
   padding-left: 10%;
+
+  width: 100%;
+  min-height: 90vh;
+  background-image: url(${Background.src});
+  background-size: cover;
 `;
 
 const LandingTitle = styled.p`
